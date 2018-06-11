42594a024dacca5b59b26f6c528e3cc143027907
hadzi96 <36916832+hadzi96@users.noreply.github.com>
2018-06-11 17:51:29 +0200

    comm

--- WEB_Project/WebContent/register.js
+++ WEB_Project/WebContent/register.js
@@ -2,16 +2,34 @@ var register = angular.module('register', []);
 
 register.controller('CtrlRegister', function($scope, $window, ServiceRegister) {
 
+	$scope.message = '';
 	$scope.register = function() {
-		var host = $window.location.host;
-        var landingUrl = "http://www.google.com";
-        $window.location.href = landingUrl;
+		
+		$scope.register = function() {
+			var user = {};
+			user = angular.copy($scope.user);
+			$scope.message = 'Registration i progress...';
+			ServiceRegister.register(user).then(function(response){
+				if(response.data == 'true'){
+					
+					$scope.message = 'Registration successfull. \nPlease check e-mail!';
+				}
+				else{
+					$scope.message = 'Registration failed!';
+				}
+			});
+		};
+		
 	};
 
 });
 
 register.factory('ServiceRegister', [ '$http', function($http) {
 	var service = {};
+	
+	service.register = function(user){
+		return $http.post('http://localhost:8080/WEB_Project/server/user/register', user);
+	}
 
 	return service;
 } ]);
